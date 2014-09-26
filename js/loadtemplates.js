595aacbd95ddee965148e4a36076ca51611fb490
matt mallett <mmallett91@gmail.com>
2014-09-26 18:46:32 -0400

    various minor tweaks. insert links, fix navbar sizing on different screen sizes

--- js/loadtemplates.js
+++ js/loadtemplates.js
@@ -1,18 +1,53 @@
 $(document).ready(function(){
 
-	loadTemplate('templates/header.hbs', '#header');
+	loadTemplate('templates/header.hbs', '#header', dynamicHeader);
 
-	loadTemplate('templates/footer.hbs', '#footer');
+	loadTemplate('templates/footer.hbs', '#footer', function(){});
 
 });
 
-function loadTemplate(template, target){
+$(window).resize(function(){
+
+	dynamicHeader();
+
+});
+
+function loadTemplate(template, target, callback){
 
 	$.get(template, function(html){
 
 		$(target).html(html);
 
+		callback();
+
 	});
+
+
+}
+
+var BASE_NAV_PADDING = 20;
+
+function dynamicHeader(){
+	// find the height of navbar from tallest a
+	var max = 0;
+	$('#nav a').each(function(){
+		max = Math.max(max, $(this).height());
+	})
+	max += BASE_NAV_PADDING;
+
+	// position a's centered within nav divs
+	$('#nav > div').height(max);
+	$('#nav a').each(function(){
+		var padding = ($('#nav > div').height() - $(this).height()) / 2;
+		$(this).parent().css('padding-top', padding);
+	})
+
+	// do the same with the logo
+	var imgPadding = 
+		(($('.logo-area .row').height() - $('.logo-area img').height()) /2).toString();
+		console.log(imgPadding);
+		console.log(imgPadding + 'px');
+	// $('.logo-area a').parent().css('padding-top', imgPadding + 'px');
 
 
 }
